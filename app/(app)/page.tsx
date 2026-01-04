2a1c55baee6de32ef804a2d6ae5ca40e0f9c9570
sai02-creator <saichaitanyagaddam1@gmail.com>
2026-01-05 00:58:19 +1100

    Add Customer document schema with email, name, Clerk ID, and Stripe ID

---
+++ app/(app)/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* Featured Products Carousel */}
+      <Button>Click Me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="link">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      {/* Page Banner */}
+
+      {/* Category Title */}
+
+      {/* Product Section */}
+    </div>
+  );
+}
