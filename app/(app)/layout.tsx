581ff521adf32964d681d47a0c16e7646fb5212e
sai02-creator <saichaitanyagaddam1@gmail.com>
2026-01-06 00:09:18 +1100

    Add <SanityLive /> component to enable live data updates

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,12 @@
+import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
           <main>{children}</main>
+          <SanityLive/>
+
     </ClerkProvider>
   );
 }
