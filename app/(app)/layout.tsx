b5ee61ed6939e768663fca9fada3611c82301260
sai02-creator <saichaitanyagaddam1@gmail.com>
2026-01-09 00:44:03 +1100

    chore: add ChatStoreProvider to app layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,15 +1,21 @@
+import { Header } from "@/components/Header";
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
+        <ChatStoreProvider>
+        <Header />
           <main>{children}</main>
           <Toaster position="bottom-center"/> 
           <SanityLive />
+          </ChatStoreProvider>
       </CartStoreProvider>
 
     </ClerkProvider>
