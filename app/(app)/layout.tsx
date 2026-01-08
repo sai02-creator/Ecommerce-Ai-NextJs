7afef500f5e1d361871ad91b0d084b3a393f8cf3
sai02-creator <saichaitanyagaddam1@gmail.com>
2026-01-08 11:26:46 +1100

    feat(cart): wrap app with CartStoreProvider

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,14 @@
+import { CartStoreProvider } from "@/lib/store/cart-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
+      <CartStoreProvider>
           <main>{children}</main>
           <SanityLive/>
+      </CartStoreProvider>
 
     </ClerkProvider>
   );
