857edfc75a8300b2c7d520e3fb6b772628804a69
sai02-creator <saichaitanyagaddam1@gmail.com>
2026-01-08 12:21:53 +1100

    "Add sonner.tsx wrapper for Toaster component to fix module import issue"

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,15 @@
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
           <main>{children}</main>
-          <SanityLive/>
+          <Toaster position="bottom-center"/> 
+          <SanityLive />
       </CartStoreProvider>
 
     </ClerkProvider>
